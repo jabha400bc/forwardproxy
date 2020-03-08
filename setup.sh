0274e425d135db0a6480c8ea1d56a3c03c883fc4
Ubuntu <ubuntu@ip-172-31-47-137.ec2.internal>
2020-03-08 09:20:17 +0000

    First Version

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x \
+&& sudo apt-get install -y apache2 apache2-doc apache2-utils \
+&& sudo apt-get install -y libxml2-dev \
+&& sudo a2enmod proxy proxy_ajp proxy_http rewrite deflate headers proxy_balancer proxy_connect proxy_html \
+&& sudo cp proxy.conf /etc/apache2/mods-available/proxy.conf \
+&& sed -i -e 's/80/8889/g' /etc/apache2/ports.conf \
+&& sudo cp -p /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf.orig \
+&& sudo cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf \
+&& sudo a2ensite 000-default.conf \
+&& set +x
